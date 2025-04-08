f6168e2e7079ca402cc9e039f151fcc3d219a896
edgdel13 <focustm138@gmail.com>
2025-04-08 15:04:45 +0200

    Initial Commit

---
+++ ProjetTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ProjetTest
+//
+//  Created by Edgar Delfly on 08/04/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ProjetTest/ProjetTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjetTestApp.swift
+//  ProjetTest
+//
+//  Created by Edgar Delfly on 08/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct ProjetTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
